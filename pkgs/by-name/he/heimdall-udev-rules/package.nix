e0aa27674f83bc7f4da76e545d8cc8a647e26b7e
nat <nat@nekopon.pl>
2025-05-13 01:28:56 +0200

    heimdall-udev-rules: init at 2.1.1

---
+++ pkgs/by-name/he/heimdall-udev-rules/package.nix
@@ -0,0 +1,25 @@
+{
+  heimdall,
+  lib,
+  runCommandLocal,
+}:
+
+runCommandLocal "heimdall-udev-rules"
+  {
+    inherit (heimdall) version src;
+
+    meta =
+      (builtins.removeAttrs heimdall.meta [
+        "broken"
+        "mainProgram"
+      ])
+      // {
+        description = "udev rules for download mode on Samsung mobile devices";
+        platforms = lib.platforms.linux;
+      };
+  }
+  ''
+    mkdir -p $out/lib/udev/rules.d
+    install -m644 -t $out/lib/udev/rules.d $src/heimdall/60-heimdall.rules
+    substituteInPlace $out/lib/udev/rules.d/60-heimdall.rules --replace-fail 'MODE="0666"' 'TAG+="uaccess"'
+  ''
