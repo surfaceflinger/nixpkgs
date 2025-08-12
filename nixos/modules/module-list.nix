aea14eb0dc87558b0063d6964c4c51a9f9383317
nat <nat@nekopon.pl>
2025-08-13 00:20:05 +0200

    nixos/zcfan: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -727,6 +727,7 @@
   ./services/hardware/usbrelayd.nix
   ./services/hardware/vdr.nix
   ./services/home-automation/deye-dummycloud.nix
+  ./services/hardware/zcfan.nix
   ./services/home-automation/ebusd.nix
   ./services/home-automation/esphome.nix
   ./services/home-automation/evcc.nix
